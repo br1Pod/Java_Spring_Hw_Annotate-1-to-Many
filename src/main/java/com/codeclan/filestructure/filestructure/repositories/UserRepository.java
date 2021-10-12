4a0347c0f618aa16e2e470e2b04af0ccea846de3
briPod <33463220+br1Pod@users.noreply.github.com>
2021-10-12 19:04:12 +0100

    User Controller written

--- src/main/java/com/codeclan/filestructure/filestructure/repositories/UserRepository.java
+++ src/main/java/com/codeclan/filestructure/filestructure/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package com.codeclan.filestructure.filestructure.repositories;
 
+import com.codeclan.filestructure.filestructure.models.User;
+
 public interface UserRepository {
+    Object findAll();
+
+    Object findById(Long id);
+
+    void save(User user);
 }
